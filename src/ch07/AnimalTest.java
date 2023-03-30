6034b79a74a275bd89ea30a4ff356aba73883fbd
User <seungm9604@gmail.com>
2023-03-30 11:15:03 +0900

    ch07

--- src/ch07/AnimalTest.java
+++ src/ch07/AnimalTest.java
@@ -1,34 +1,5 @@
 package ch07;
 
-class Animal {
-    private double weight;
-    private String picture;
-
-    protected void eat() {
-        System.out.println("eat()이 호출되었음");
-    }
-
-    void sleep() {
-        System.out.println("sleep()이 호출되었음");
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-}
-
 class Lion extends Animal {
     private int legs = 4;
 
