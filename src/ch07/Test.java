c4cfe6ebad70c1fb493c296c2cdc8c1aa0eb2e92
User <seungm9604@gmail.com>
2023-03-30 12:08:36 +0900

    ch07

--- src/ch07/Test.java
+++ src/ch07/Test.java
@@ -3,5 +3,6 @@ package ch07;
 public class Test {
     public static void main(String[] args) {
         Derived d = new Derived();
+        // Derived d = new Derived("홍길동"); 오류 발생 - 생성자에 String 매개변수가 없기 때문
     }
 }
