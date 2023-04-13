ef6d1f8daa7375a2d4cf74d2a07959abe0637c46
User <seungm9604@gmail.com>
2023-04-13 12:37:33 +0900

    Override

--- src/ch07/objectClass/Car.java
+++ src/ch07/objectClass/Car.java
@@ -16,7 +16,13 @@ public class Car {
     @Override
     public boolean equals(Object obj) {
         if(obj == null) return false;
+        // ojb 가 Car 클래스의 인스턴스가 아니면 바로 false 를 반환
+        // 자바에는 이 목적으로 사용할 수 있는 연산자가 있다.
+        // instanceof
+        if(obj instanceof Car == false) return false;
+
         // obj 는 Car 클래스의 객체라 가정
+        // 하향 형변환
         Car yourCar = (Car)obj;
         if(yourCar.brand.equals(brand) && yourCar.modelName.equals(modelName))
             return true;
